038a3b2c8204b43774a3ddea277a0113c5e4a7ab
ddmills <mills.dalton@gmail.com>
2016-10-23 23:20:09 -0500

    add more wedding party

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,19 @@
 import 'babel-polyfill';
 import scrollMonitor from 'scrollmonitor';
 
-const tiles = document.getElementsByClassName('tile');
+const profilesLeft = document.querySelectorAll('.profiles-left .profile');
+const profilesRight = document.querySelectorAll('.profiles-right .profile');
 
-for (let tile of tiles) {
-  const watcher = scrollMonitor.create(tile);
-  watcher.enterViewport(() => {
-    tile.classList.add('fade-in-left');
+for (let profile of profilesLeft) {
+  const watcher = scrollMonitor.create(profile);
+  watcher.one('enterViewport', () => {
+    profile.classList.add('fade-in-left');
+  });
+};
+
+for (let profile of profilesRight) {
+  const watcher = scrollMonitor.create(profile);
+  watcher.one('enterViewport', () => {
+    profile.classList.add('fade-in-right');
   });
 };
